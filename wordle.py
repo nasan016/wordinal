3054ab8409edb9289b92550e2cbe49cbfe101b38
Nathaniel Macapinlac <83597466+nasan016@users.noreply.github.com>
2022-04-10 22:20:18 -0400

    made it a little better

--- wordle.py
+++ wordle.py
@@ -1,115 +1,138 @@
 from random import randint
 from os import system
+import platform
 
-system('cls')
-
-words = open('words.txt', 'r').readlines()
-
-random_integer = randint(0, len(words) - 1)
-random_word = (words[random_integer]).strip('\n').upper()
-random_word_temp = []
-[random_word_temp.append(j) for j in random_word]
-
-
-available_letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L'
-                     ,'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
-                     'X', 'Y', 'Z']
-letters_used = []
-
-num_guess = 6
-
-green_square = '\U0001f7e9' #Correct Letter/Place
-red_square = '\U0001f7e5' #Wrong Letter
-yellow_square = '\U0001f7e8' #Correct Letter
-
-prev = []
-
-
-ending = 'You Lose! The word was {}.'.format(random_word)
-print("\nWelcome to Wordinal!\n ")
-
-while num_guess != 0:
-
-    letter_1 = red_square
-    letter_2 = red_square
-    letter_3 = red_square
-    letter_4 = red_square
-    letter_5 = red_square
-
-    status = '{}{}{}{}{}'.format(letter_1, letter_2, letter_3, letter_4,
-                                 letter_5)
-    print('Letter Bank: ' + (' '.join(str(x) for x in available_letters)))
-    print('Letters Used: ' + ' '.join(str(k) for k in letters_used))
-    print('Previous Guess:')
-
-    #Append previous guess and previous status, print to one line both.
-
-    print(('\n'.join(str(o) for o in prev)))
-
-
-
-    guess = input('\nGuess: ')[:5].upper()
-
-
-    if guess == random_word:
-        ending = 'You Win!'
-        break
+#------------------------------------------------------------------------
+def clearScreen():
+    operating_system = platform.system()
+    if operating_system == 'Windows':
+        system('cls')
+    elif operating_system == 'Darwin' or operating_system == 'Linux':
+        system('clear')
+#------------------------------------------------------------------------
+def isPlaying():
+    flag = True
+    while flag:
+        is_playing = input('(y/n): ')
+        if is_playing == 'y' or is_playing == 'n':
+            flag = False
+        
+    if is_playing == 'y':
+        game()
     else:
-        for i in range(len(guess)):
-            if guess[i] in available_letters:
-                index = available_letters.index(guess[i])
-                del available_letters[index]
-            if guess[i] not in letters_used:
-                letters_used.append(guess[i])
+        end_screen()
 
-            if guess[i] in random_word_temp:
-                if i == 0:
-                    if guess[i] in random_word_temp:
-                        letter_1 = yellow_square
-                    if guess[i] == random_word_temp[i]:
-                        letter_1 = green_square
+def game():
+    clearScreen()
+    word = getRandomWord()
+    guess_squares = []
+    ending = f'You Lose! The word was {word}'
+    number_of_guesses = 6
+    letters = []
 
-                elif i == 1:
-                    if guess[i] in random_word:
-                        letter_2 = yellow_square
-                    if guess[i] == random_word_temp[i]:
-                        letter_2 = green_square
+    while number_of_guesses != 0:
+        guess = input('Guess: ').lower()
+        if guessCheck(guess):
+            if guess == word:
+                ending = 'You Win!'
+                break
+            else:
+                squares = (guessInWord(guess, word))
+                prev_guess = (guess)
+                prev_guess_squares = f'{prev_guess} {squares}'
+                guess_squares.append(prev_guess_squares)
+            number_of_guesses -= 1
+        else:
+            print('INVALID WORD')
+            cont = input()
 
-                elif i == 2:
-                    if guess[i] in random_word_temp:
-                        letter_3 = yellow_square
-                    if guess[i] == random_word_temp[i]:
-                        letter_3 = green_square
-
-                elif i == 3:
-                    if guess[i] in random_word_temp:
-                        letter_4 = yellow_square
-                    if guess[i] == random_word_temp[i]:
-                        letter_4 = green_square
-
-
-                elif i == 4:
-                    if guess[i] in random_word_temp:
-                        letter_5 = yellow_square
-                    if guess[i] == random_word_temp[i]:
-                        letter_5 = green_square
-
-
-    status = '{}{}{}{}{}'.format(letter_1, letter_2,
-                                 letter_3, letter_4, letter_5)
-
-
-    prev_guess_status = '{} {}'.format(guess, status)
-    prev.append(prev_guess_status)
-    letters_used.sort()
-
-    num_guess -= 1
+        clearScreen()
+        for x in get_letters(guess, word):
+            if x not in letters:
+                letters.append(x)
+        print('Letters in random word: ' + ' '.join(str(y) for y in letters))
+        print('\n'.join(guess_squares))
     
-    if num_guess == 0:
-      break
+    print(ending)
+    print("Would you like to play again?")
+    isPlaying()
 
-    system('cls')
+def getRandomWord():
+    with open('words.txt') as f:
+        words = f.read().splitlines()
+    rand_int = randint(0, len(words) - 1)
 
-print(ending)
+    return words[rand_int]
 
+def guessCheck(guess):
+    with open('all-words.txt') as f:
+        words = f.read().splitlines()
 
+    if guess in words:
+        return True
+
+def guessInWord(guess, word):
+    
+    green_square = '\U0001f7e9' #Correct Letter/Place
+    red_square = '\U0001f7e5' #Wrong Letter
+    yellow_square = '\U0001f7e8' #Correct Letter
+
+    square_1, square_2, square_3, square_4, square_5 = red_square, red_square, red_square, red_square, red_square
+
+    for i in range(len(guess)):
+        if guess[i] in word:
+            if i == 0:
+                square_1 = yellow_square
+            elif i == 1:
+                square_2 = yellow_square
+            elif i == 2:
+                square_3 = yellow_square
+            elif i == 3:
+                square_4 = yellow_square
+            elif i == 4:
+                square_5 = yellow_square
+            
+            if guess[i] == word[i]:
+                if i == 0:
+                    square_1 = green_square
+                elif i == 1:
+                    square_2 = green_square
+                elif i == 2:
+                    square_3 = green_square
+                elif i == 3:
+                    square_4 = green_square
+                elif i == 4:
+                    square_5 = green_square
+            
+
+    squares = square_1 + square_2 + square_3 + square_4 + square_5
+    
+    return squares
+
+def get_letters(guess, word):
+    letters = []
+    for i in range(len(guess)):
+        if guess[i] in word:
+            if guess[i] not in letters:
+                letters.append(guess[i])
+
+    letters.sort()
+
+    return letters
+#-------------------------------------------------------------------
+def end_screen():
+    clearScreen()
+    print(
+    """
+    Thank you for playing!
+    Wordinal (Wordle Clone)
+    Created By: nasan016
+    """
+    )
+#------------------------------------------------------------------
+def main():
+    print("Welcome to Wordinal! Would you like to play?")
+    isPlaying()
+
+if __name__ == '__main__':
+    main()
